1d36f30a28ece311ca8fa3be68eb0dee0d38f2d4
fvnny-code <f.codecco.grando@gmail.com>
2022-12-09 18:54:27 +0100

    * index.html : removal of useless or duplicated elements. * modal.css : reorganisation of the code for lisibility improvment + comment(before removal) of unnecessary classes and styling, so as to match the mockup. * modal.js : first steps of function validate() implementation -> confirmation dialog appears before form validation.

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,16 +8,40 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
+const modalbg = document.querySelector(".modal-form");
+const modalConfirm = document.querySelector(".modal-confirmation");
 const modalBtn = document.querySelectorAll(".modal-btn");
+// Verifier les valeurs regex  ou if
 const formData = document.querySelectorAll(".formData");
+
+console.log(modalBtn);
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
+  console.log("lol");
   modalbg.style.display = "block";
 }
 
+function validate() {
+  //console.log("blourp");
+  // conditions sur les inputs
+
+  // Si problème
+  // if (...) { elem.setAttribute("data-error-visible, true); return false; }
+  //ces attributs sont dans le css .formData l-223 et 226
+
+
+  modalbg.style.display = "none";
+  modalConfirm.style.display = "block";
+
+
+
+
+
+  return false; // empêche la validation
+ 
+}
 
